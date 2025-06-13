dbb33cfec623ea5e03403ee49666599a1d9ff2df
jeelbhanavdiyagnx <147592549+jeelbhanavdiyagnx@users.noreply.github.com>
2025-06-13 15:14:42 +0530

    uncomment code that is used for the branch reducer

--- redux/reducers/branchReducer.js
+++ redux/reducers/branchReducer.js
@@ -12,22 +12,22 @@ const initialState = {
   selectedBranchId: null
 };
 
-// const branchReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case FETCH_BRANCHES_REQUEST:
-//       return { ...state, loading: true };
-//     case FETCH_BRANCHES_SUCCESS:
-//       return { ...state, loading: false, branches: action.payload };
-//     case FETCH_BRANCHES_FAILURE:
-//       return { ...state, loading: false, error: action.payload };
-//     case SET_SELECTED_BRANCH:
-//       return {
-//         ...state,
-//         selectedBranchId: action.payload
-//       };
-//     default:
-//       return state;
-//   }
-// };
+const branchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_BRANCHES_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_BRANCHES_SUCCESS:
+      return { ...state, loading: false, branches: action.payload };
+    case FETCH_BRANCHES_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    case SET_SELECTED_BRANCH:
+      return {
+        ...state,
+        selectedBranchId: action.payload
+      };
+    default:
+      return state;
+  }
+};
 
 export default branchReducer;
