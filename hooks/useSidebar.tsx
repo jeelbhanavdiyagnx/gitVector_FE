e7a772066fbaf78b96bd846f336fe01aab2f0a26
jeelbhanavdiyagnx <147592549+jeelbhanavdiyagnx@users.noreply.github.com>
2025-06-18 17:06:09 +0530

    Update useSidebar.tsx

--- hooks/useSidebar.tsx
+++ hooks/useSidebar.tsx
@@ -4,7 +4,6 @@ interface SidebarStore {
   isMinimized: boolean;
   toggle: () => void;
 }
-
 export const useSidebar = create<SidebarStore>((set) => ({
   isMinimized: false,
   toggle: () => set((state) => ({ isMinimized: !state.isMinimized }))
