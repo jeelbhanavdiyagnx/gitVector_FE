fd4861950c59a727712e3e53262c32ef36beba13
jeelbhanavdiyagnx <147592549+jeelbhanavdiyagnx@users.noreply.github.com>
2025-06-19 14:38:05 +0530

    Update ReduxProvider.js

--- app/ReduxProvider.js
+++ app/ReduxProvider.js
@@ -3,8 +3,5 @@
 import { Provider } from 'react-redux';
 import store from '@/redux/store'; // Adjust the path to your store
 
-const ReduxProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
 
 export default ReduxProvider;
